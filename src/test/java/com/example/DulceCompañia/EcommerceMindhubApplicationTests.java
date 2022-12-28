8e11bd734899d194bc24c0acc1efa4382de97949
david garcia <115822539+LikidPro@users.noreply.github.com>
2022-12-28 14:51:06 -0300

    Add files via upload

---
+++ src/test/java/com/example/DulceCompañia/EcommerceMindhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DulceCompañia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMindhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
